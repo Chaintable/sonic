bd432dd3e5393e5c1f1365f0cb02e08958b175df
plopl666 <wangchengao@outlook.com>
2025-09-18 02:19:43 +0900

    debug

--- evmcore/state_processor.go
+++ evmcore/state_processor.go
@@ -203,6 +203,7 @@ func ApplyTransactionWithEVM(msg *core.Message, config *params.ChainConfig, gp *
 		evm.Config.Tracer.OnTxStart(evm.GetVMContext(), tx, msg.From)
 		if evm.Config.Tracer.OnTxEnd != nil {
 			defer func() {
+				log.Info("Transaction end", "tx", tx.Hash().Hex(), "err", err, "receipt", receipt)
 				evm.Config.Tracer.OnTxEnd(receipt, err)
 			}()
 		}
